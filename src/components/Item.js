c7f522ea4a7d3b29e55760b1b539bb5d59821494
Vivian Gichure <viviangichure@gmail.com>
2025-07-11 19:56:20 +0300

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,19 +3,20 @@ import React, { useState } from "react";
 function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false);
 
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+  function toggleCartStatus() {
+    setIsInCart((prevState) => !prevState);
   }
 
+  const itemClass = isInCart ? "in-cart" : "";
+  const buttonClass = isInCart ? "remove" : "add";
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className={isInCart ? "in-cart" : ""}>
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
+      <button className={buttonClass} onClick={toggleCartStatus}>
+        {buttonText}
       </button>
     </li>
   );
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,36 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
+  const [itemList, setItemList] = useState(items); // Use local state for items
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    setSearchText(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  function handleItemFormSubmit(newItem) {
+    setItemList([...itemList, newItem]);
+  }
+
+  const itemsToDisplay = itemList.filter((item) => {
+    const categoryMatch =
+      selectedCategory === "All" || item.category === selectedCategory;
+    const nameMatch = item.name.toLowerCase().includes(searchText.toLowerCase());
+    return categoryMatch && nameMatch;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+        searchText={searchText}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
